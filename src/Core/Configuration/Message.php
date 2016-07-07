c99857decea916f9d1868207d77dc90dfb052b43
Tom <ethnovode@gmail.com>
2016-07-07 14:24:05 +0200

    Update Message.php

--- src/Core/Configuration/Message.php
+++ src/Core/Configuration/Message.php
@@ -3,34 +3,35 @@
 require_once(PATH_SDK_ROOT . 'Core/Configuration/Request.php');
 require_once(PATH_SDK_ROOT . 'Core/Configuration/Response.php');
 
-/**
- * Contains properties about the Request and Response configuration settings.
- */
-class Message
-{
-	/**
-	 * Request configuration settings
-	 * @var Request 
-	 */
-	public $Request;
-
-	/**
-	 * Response configuration settings
-	 * @var Response 
-	 */
-	public $Response;
-	
-	/**
-	 * Initializes a new instance of the Message class.
-	 *
-	 * @param Request $Request Request configuration settings
-	 * @param Response $Response Response configuration settings
-	 */
-	public function __construct($Request=NULL, $Response=NULL)
-	{
-		$this->Request = $Request;
-		$this->Response = $Response;
-	}
-}
+// Conflict with drupal message module. Needs a fix.
+///**
+// * Contains properties about the Request and Response configuration settings.
+// */
+//class Message
+//{
+//	/**
+//	 * Request configuration settings
+//	 * @var Request 
+//	 */
+//	public $Request;
+//
+//	/**
+//	 * Response configuration settings
+//	 * @var Response 
+//	 */
+//	public $Response;
+//	
+//	/**
+//	 * Initializes a new instance of the Message class.
+//	 *
+//	 * @param Request $Request Request configuration settings
+//	 * @param Response $Response Response configuration settings
+//	 */
+//	public function __construct($Request=NULL, $Response=NULL)
+//	{
+//		$this->Request = $Request;
+//		$this->Response = $Response;
+//	}
+//}
 
 ?>
